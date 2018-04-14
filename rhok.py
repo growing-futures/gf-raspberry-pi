b5678223dac28bbcd6b51c9fcc5445464924ee5e
charles <charles@local.com>
2018-04-14 11:24:03 -0400

    Added serial read/print of data.

--- rhok.py
+++ rhok.py
@@ -1,1 +1,13 @@
 import serial
+import time
+
+# Use this cmd to get the dev name of the arduino
+# ls /dev/tty*
+ser = serial.Serial('/dev/ttyACM0', 9600)
+
+while True:
+    sensor_data = ser.readline()
+    # Convert byte array to a string.
+    sensor_data = str(sensor_data).split(',')
+    print(sensor_data)
+    #time.sleep(0.5)
