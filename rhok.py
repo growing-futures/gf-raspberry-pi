3ea391aa88c1808baad1118806398cc8b09c791f
charles <charles@local.com>
2018-04-14 20:06:31 -0400

    adding config file support

--- rhok.py
+++ rhok.py
@@ -1,29 +1,39 @@
 #import logging
 from enum import Enum, unique
 from influxdb import InfluxDBClient
+from influxdb.exceptions import InfluxDBClientError
 import json
 #from sensor import sensor_fields
 import serial  # For communication with arduino.
 import time
 
 
-hostName = 'growingfuturesapp.ca'
-hostPort = 8086
-dbname = 'gf'
+MEASUREMENT = 'measurement'
+TAGS = 'tags'
+DB = 'db'
+ARDUINO = 'arduino'
+CONFIG_KEYS = (MEASUREMENT, TAGS, DB, ARDUINO)
+#CONFIG_KEYS = (MEASUREMENT, TAGS, FIELDS, DB, ARDUINO)
+
+FIELDS = 'fields'
+
+F_WATER_LEVEL = "water_level"
+F_AIR_HUMIDITY = "air_humidity"
+F_AIR_TEMP = "air_temp"
+F_WATER_TEMP = "water_temp"
+F_PH = "pH"
+F_LIGHT_STATUS_1 = "light_status_1"
+F_LIGHT_STATUS_2 = "light_status_2"
+F_LIGHT_STATUS_3 = "light_status_3"
+F_LIGHT_STATUS_4 = "light_status_4"
 
 
-# TODO - hardcoded for now
-tower_name = 60
-tower_group = "Tower 60 Postal Office"
 
+WL = "water_level"
+WL_SENSOR_HEIGHT = "sensor_height"
+WL_MAX = "max_water_level"
+WL_MIN = "min_water_level"
 
-#sensor_source = ["measurement", "tags", "fields"]
-#
-#sensor_tags = [
-#        "towerName",
-#        "towerGroup"
-#]
-#
 
 @unique
 class LightStatus(Enum):
@@ -40,57 +50,82 @@ def to_float(s):
         # TODO
         return s
 
-def to_str(s):
-    return s
+
+def to_str(s): return s
+
 
 def to_light_status(light_data):
+    """Used to convert light sensor data to light status."""
     # TODO - convert to enum
     if 1 == light_data: status = LightStatus.on
     else: status =  LightStatus.off
     return to_float(status.value)
 
 
-sensor_fields = [
-        ('water_level', to_float),
-        ('air_humidity', to_float),
-        ('air_temp', to_float),
-        ('water_temp', to_float),
-        ('pH', to_float),
+def create_to_water_level(config_data):
+    """Used to create the 'to_water_level' func."""
+    wl_config = config_data[WL]
+    def to_water_level(sensor_value, height=wl_config[WL_SENSOR_HEIGHT],
+            wl_max=wl_config[WL_MAX], wl_min=wl_config[WL_MIN]):
+        sensor_value = to_float(sensor_value)
+        print(height, wl_max, wl_min, sensor_value)
+        return (height - sensor_value - wl_min / (wl_max - wl_min)) * 100
+    return to_water_level
 
-        # Field values: 0, 1, 'x'.
-        # Some setups have less than 4 lights, so 'x' is used as ignore
-        # or not applicable.
-        ('light_status_1', to_light_status),
-        ('light_status_2', to_light_status),
-        ('light_status_3', to_light_status),
-        ('light_status_4', to_light_status),
-]
 
-sensor_fields_len = len(sensor_fields)
+# Required as the arduino sends us the data in this order.
+FIELD_ORDER = (
+        F_WATER_LEVEL,
+        F_AIR_HUMIDITY,
+        F_AIR_TEMP,
+        F_WATER_TEMP,
+        F_PH,
+        F_LIGHT_STATUS_1,
+        F_LIGHT_STATUS_2,
+        F_LIGHT_STATUS_3,
+        F_LIGHT_STATUS_4,
+)
 
-# Ref: https://oscarliang.com/connect-raspberry-pi-and-arduino-usb-cable/
+FIELDS_LEN = len(FIELD_ORDER)
 
-def to_dict(sensor_data):
+
+
+def to_dict(config_data, field_dict, sensor_data):
+    """Used to convert the sensor data into a dict for easy conversion to
+    json format.
+    """
     # TODO - have dict already created, only need to populate with new sensor
     # data
     d = {}
-    d['measurement'] = 'TowerData'
 
-    tags = {}
-    tags['towerName'] = 'Tower_{}'.format(tower_name)
-    tags['towerGroup'] = 'Tower_Group_{}'.format(tower_group)
-    d['tags'] = tags
+    # Measurement data.
+    d[MEASUREMENT] = config_data[MEASUREMENT]
+    d[TAGS] = dict(config_data[TAGS])
+
     fields = {}
 
-    for e, (field, func) in enumerate(sensor_fields):
+    for e, field in enumerate(FIELD_ORDER):
         # We ignore 'x' fields. The arduino is sending 4 light statuses even
         # if they only have 1 light. We filter out any that don't have data.
+        convert_func = field_dict.get(field, to_str)
         data = sensor_data[e]
         if 'x' == data: continue
-        fields[field] = func(data)
+        fields[field] = convert_func(data)
 
-    d['fields'] = fields
+    d[FIELDS] = fields
     return d
+
+
+def get_config_data(filename):
+    """Used to read the json config data."""
+    with open(filename) as fp:
+        config_data = json.load(fp)
+
+    # Do some basic checking.
+    for key in CONFIG_KEYS:
+        if key not in config_data:
+            print('ERROR: Missing config.json key "{}"'.format(key))
+    return config_data
 
 
 def main():
@@ -98,28 +133,42 @@ def main():
     # be a bunch of tty devices. It is usually something like ttyACM0
     # or ttypUSB0. The last number is dependant on the usb port being used.
     # ls /dev/tty*
-    ser = serial.Serial('/dev/ttyACM1', 9600)
-    client = InfluxDBClient(host=hostName, port=hostPort, username='gfsensor',
+    ser = serial.Serial('/dev/ttyACM1', baud_rate)
+    client = InfluxDBClient(host=host_name, port=host_port, username='gfsensor',
             password='rhokmonitoring', ssl=True, verify_ssl=True)
     client.switch_database(dbname)
 
-    #sensor_dict = {f:0.0 for f in sensor_fields}
+    # Read in the config data.
+    config_data = get_config_data(CONFIG_FILENAME)
+    print(config_data)
+
+    field_dict = {
+            F_WATER_LEVEL : create_to_water_level(config_data),
+            F_AIR_HUMIDITY : to_float,
+            F_AIR_TEMP : to_float,
+            F_WATER_TEMP : to_float,
+            F_PH : to_float,
+            F_LIGHT_STATUS_1 : to_light_status,
+            F_LIGHT_STATUS_2 : to_light_status,
+            F_LIGHT_STATUS_3 : to_light_status,
+            F_LIGHT_STATUS_4 : to_light_status,
+    }
 
     while True:
         try:
             sensor_data = ser.readline()
-        except serial.SerialException:
+        except serial.SerialException as e:
             # One reason this can occur is when the rpi is disconnected from the
             # arduino.
             # TODO
-            #logger.error('SerialError')
-            pass
+            print('Exception: {}'.format(e))
+            break
 
         # Convert byte array to a string.
         sensor_data = sensor_data.decode('utf-8').strip().split(',')
-        print(sensor_data)
+        #print(sensor_data)
 
-        if len(sensor_data) != sensor_fields_len:
+        if len(sensor_data) != FIELDS_LEN:
             # TODO - error - drop data?
             print('WARNING: Sensor data length mismatch (ignoring sensor '
                     'data), received {} values, expecting {} values'.format(
@@ -131,11 +180,14 @@ def main():
         #logger.info(sensor_data)
 
         # Output to json.
-        d = [to_dict(sensor_data)]
+        d = [to_dict(config_data, field_dict, sensor_data)]
         print(json.dumps(d))
 
-        if client.write_points(d):
-            print("Insert success")
+        try:
+            if not client.write_points(d):
+                print('Failed client data write: {}'.format(d))
+        except InfluxDBClientError as e:
+            print('Exception: {}'.format(e))
 
 
 if '__main__' == __name__:
